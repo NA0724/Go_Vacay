10c83d5685055695cddf0abc47ddb8768a5024ea
Neha Raj <neharaj710@gmail.com>
2023-05-09 10:20:22 -0700

    config changes

--- internal/config/config.go
+++ internal/config/config.go
@@ -7,11 +7,14 @@ import (
 	"github.com/alexedwards/scs/v2"
 )
 
+var path = "/Users/neharaj/Go_Vacay/"
+
 // AppConfig holds the application config
 type AppConfig struct {
 	UseCache      bool
 	TemplateCache map[string]*template.Template
 	InfoLog       *log.Logger
+	ErrorLog      *log.Logger
 	InProd        bool
 	Session       *scs.SessionManager
 }
